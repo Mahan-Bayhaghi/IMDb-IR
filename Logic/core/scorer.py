b624dfe11347d3543d08750f1fbc338aa7b01cbc
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2024-04-03 22:23:20 +0330

    vector space model score added

--- Logic/core/scorer.py
+++ Logic/core/scorer.py
@@ -1,6 +1,12 @@
+import collections
+import math
+
 import numpy as np
 
-class Scorer:    
+from indexer import index_reader, indexes_enum
+
+
+class Scorer:
     def __init__(self, index, number_of_documents):
         """
         Initializes the Scorer.
@@ -17,7 +23,7 @@ class Scorer:
         self.idf = {}
         self.N = number_of_documents
 
-    def get_list_of_documents(self,query):
+    def get_list_of_documents(self, query):
         """
         Returns a list of documents that contain at least one of the terms in the query.
 
@@ -44,7 +50,7 @@ class Scorer:
             if term in self.index.keys():
                 list_of_documents.extend(self.index[term].keys())
         return list(set(list_of_documents))
-    
+
     def get_idf(self, term):
         """
         Returns the inverse document frequency of a term.
@@ -63,12 +69,16 @@ class Scorer:
         -------
             It was better to store dfs in a separate dict in preprocessing.
         """
+        # TODO
         idf = self.idf.get(term, None)
         if idf is None:
-            # TODO
-            pass
+            df = len(self.index.get(term).keys())
+            idf = np.log(self.N / df)
+            self.idf[term] = idf
+
+        print(f"idf of term {term} is {idf}")
         return idf
-    
+
     def get_query_tfs(self, query):
         """
         Returns the term frequencies of the terms in the query.
@@ -83,9 +93,11 @@ class Scorer:
         dict
             A dictionary of the term frequencies of the terms in the query.
         """
-        
-        #TODO
-
+        # TODO
+        query_tfs = collections.defaultdict(int)
+        for term in query:
+            query_tfs[term] += 1
+        return query_tfs
 
     def compute_scores_with_vector_space_model(self, query, method):
         """
@@ -105,7 +117,13 @@ class Scorer:
         """
 
         # TODO
-        pass
+        document_method, query_method = method[:3], method[4:]
+        scores = {}
+        for document_id in self.get_list_of_documents(query):  # get all documents having at least one word of bag
+            query_tfs = self.get_query_tfs(query)
+            scores[document_id] = self.get_vector_space_model_score(query, query_tfs, document_id, document_method,
+                                                                    query_method)
+        return scores
 
     def get_vector_space_model_score(self, query, query_tfs, document_id, document_method, query_method):
         """
@@ -130,8 +148,45 @@ class Scorer:
             The Vector Space Model score of the document for the query.
         """
 
-        #TODO
-        pass
+        # TODO
+        document_vector = []
+        query_vector = []
+
+        for term in query:
+            # tf handling for document
+            tf_in_document = self.index.get(term, {}).get(document_id, 0)  # TODO: search in index using Trie
+            if document_method[0] == 'l':
+                tf_in_document = np.log(tf_in_document + 1)
+            # df handling for document
+            df_in_document = 1
+            if document_method[1] == 't':
+                df_in_document = self.get_idf(term)
+            document_vector.append(tf_in_document * df_in_document)
+
+            # tf handling for query
+            tf_in_query = query_tfs[term]
+            if query_method[0] == 'l':
+                tf_in_query = np.log(tf_in_query + 1)
+            # df handling for query
+            df_in_query = 1
+            if query[1] == 't':
+                df_in_query = self.get_idf(term)
+            query_vector.append(tf_in_query * df_in_query)
+
+        if document_method[2] == 'c':
+            document_vector = np.array(self.cosine_normalize(document_vector))
+        if query_method[2] == 'c':
+            query_vector = np.array(self.cosine_normalize(query_vector))
+
+        score = np.dot(document_vector, query_vector)
+        return score
+
+    def cosine_normalize(self, vector: list):
+        w = 0.0
+        for v in vector:
+            w += v ** 2
+        w = math.sqrt(w)
+        return [v / w for v in vector]
 
     def compute_socres_with_okapi_bm25(self, query, average_document_field_length, document_lengths):
         """
@@ -180,3 +235,24 @@ class Scorer:
 
         # TODO
         pass
+
+
+def main():
+    query = "spider man in wonderland"
+    method = "lnc.ltn"
+    path = "D:/Sharif/Daneshgah stuff/term 6/mir/project/IMDb-IR/Logic/core/indexer/saved_indexes/"
+    reader = index_reader.Index_reader(path, indexes_enum.Indexes.SUMMARIES)
+    query = "peter parker"
+    query_as_list = query.split()
+    scorer = Scorer(reader.index, 3253)
+    print(scorer.get_list_of_documents(query_as_list))
+    res = scorer.compute_scores_with_vector_space_model(query_as_list, method)
+    res_as_list = []
+    for k, v in res.items():
+        res_as_list.append((k, v))
+    res_as_list.sort(key=lambda x: x[1], reverse=True)
+    print(res_as_list)
+
+
+if __name__ == "__main__":
+    main()
