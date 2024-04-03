8caaf8c1029f777f6fb32057e9ce3bb67753007e
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2024-04-03 10:30:00 +0330

    added metadata_index

--- Logic/core/indexer/metadata_index.py
+++ Logic/core/indexer/metadata_index.py
@@ -2,8 +2,9 @@ from index_reader import Index_reader
 from indexes_enum import Indexes, Index_types
 import json
 
+
 class Metadata_index:
-    def __init__(self, path='index/'):
+    def __init__(self, path='./saved_indexes/'):
         """
         Initializes the Metadata_index.
 
@@ -12,42 +13,51 @@ class Metadata_index:
         path : str
             The path to the indexes.
         """
-        
-        #TODO
+        # TODO
+        self.path = path
+        self.documents = self.read_documents()
+        self.metadata_index = None
+        self.create_metadata_index()
+        self.store_metadata_index(path)
 
     def read_documents(self):
         """
         Reads the documents.
         
         """
+        # TODO
+        document_index = Index_reader(self.path, index_name=Indexes.DOCUMENTS).index
+        return document_index
 
-        #TODO
-
-    def create_metadata_index(self):    
+    def create_metadata_index(self):
         """
         Creates the metadata index.
         """
-        metadata_index = {}
-        metadata_index['averge_document_length'] = {
+        metadata_index = {'average_document_length': {
             'stars': self.get_average_document_field_length('stars'),
             'genres': self.get_average_document_field_length('genres'),
             'summaries': self.get_average_document_field_length('summaries')
-        }
-        metadata_index['document_count'] = len(self.documents)
-
+        }, 'document_count': len(self.documents)}
+        self.metadata_index = metadata_index
         return metadata_index
-    
-    def get_average_document_field_length(self,where):
+
+    def get_average_document_field_length(self, where):
         """
-        Returns the sum of the field lengths of all documents in the index.
+        Returns the average of the field lengths of all documents in the index.
 
         Parameters
         ----------
         where : str
             The field to get the document lengths for.
         """
+        # TODO
+        total, counter = 0, 0
+        for document_id, document in self.documents.items():
+            if where in document:
+                total += len(document[where])
+                counter += 1
 
-        #TODO
+        return 0 if counter == 0 else (float(total)/float(counter))
 
     def store_metadata_index(self, path):
         """
@@ -58,11 +68,10 @@ class Metadata_index:
         path : str
             The path to the directory where the indexes are stored.
         """
-        path =  path + Indexes.DOCUMENTS.value + '_' + Index_types.METADATA.value + '_index.json'
+        path = path + Indexes.DOCUMENTS.value + '_' + Index_types.METADATA.value + '_index.json'
         with open(path, 'w') as file:
             json.dump(self.metadata_index, file, indent=4)
 
 
-    
 if __name__ == "__main__":
     meta_index = Metadata_index()
