bd6f43532517def36236372defde09863b14d3ac
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2024-05-16 12:55:50 +0330

    fasttext data loader added, training and analogy works (analogy seems week)

--- Logic/core/word_embedding/fasttext_data_loader.py
+++ Logic/core/word_embedding/fasttext_data_loader.py
@@ -1,6 +1,59 @@
+import itertools
+import re
+
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from sklearn.preprocessing import LabelEncoder
+from Logic.core.indexer.index_reader import Index_reader
+from Logic.core.indexer.indexes_enum import Indexes
+import Logic.core.path_access as path_access
+from tqdm import tqdm
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from scipy.spatial import distance
+
+
+def preprocess_text(text, minimum_length=1, stopword_removal=True, stopwords_domain=[], lower_case=True,
+                    punctuation_removal=True, str_output=True):
+    """
+    preprocess text by removing stopwords, punctuations, and converting to lowercase, and also filter based on a min length
+    for stopwords use nltk.corpus.stopwords.words('english')
+    for punctuations use string.punctuation
+
+    Parameters
+    ----------
+    text: str
+        text to be preprocessed
+    minimum_length: int
+        minimum length of the token
+    stopword_removal: bool
+        whether to remove stopwords
+    stopwords_domain: list
+        list of stopwords to be removed base on domain
+    lower_case: bool
+        whether to convert to lowercase
+    punctuation_removal: bool
+        whether to remove punctuations
+    """
+    pass
+    if pd.isna(text):
+        text = ""
+    if lower_case:
+        text = text.lower()
+    if punctuation_removal:
+        text = re.sub(r'[^\w\s]', '', text)
+        text = re.sub(r',', ' ', text)
+    if stopword_removal:
+        stopwords_ = set(stopwords.words("english"))
+        tokens = word_tokenize(text)
+        text = [word for word in tokens if word not in stopwords_]
+    if minimum_length > 1:
+        text = [word for word in text if len(word) >= minimum_length]
+
+    if str_output:
+        return " ".join(text)
+    return text
 
 
 class FastTextDataLoader:
@@ -20,7 +73,7 @@ class FastTextDataLoader:
             The path to the file containing movie information.
         """
         self.file_path = file_path
-        pass
+
 
     def read_data_to_df(self):
         """
@@ -34,7 +87,39 @@ class FastTextDataLoader:
         ----------
             pd.DataFrame: A pandas DataFrame containing movie information (synopses, summaries, reviews, titles, genres).
         """
-        pass
+        index = Index_reader(path=self.file_path, index_name=Indexes.DOCUMENTS).index
+        # print(f"Index is {index}")
+        synopses = []
+        summaries = []
+        reviews = []
+        titles = []
+        genres = []
+        # counter = 0
+        for movie_id, movie in index.items():
+            # counter += 1
+            # if counter == 50:
+            #     break
+
+            # print("snp is ", ))
+            titles.append(preprocess_text((movie.get("title", ""))))
+            genres.append(preprocess_text(" ".join(movie.get("genres", []))))
+            synopses.append(preprocess_text(" ".join(movie["synposis"])))
+            summaries.append(preprocess_text(" ".join(movie["summaries"])))
+
+            all_reviews = movie.get("reviews")
+            rev = []
+            for review in all_reviews:
+                rev.append(review[0])
+                rev.append(review[1])
+            reviews.append(preprocess_text(" ".join(rev)))
+
+        return pd.DataFrame({
+            'title': titles,
+            'genre': genres,
+            'synopsis': synopses,
+            'summary': summaries,
+            'reviews': reviews
+        })
 
     def create_train_data(self):
         """
@@ -43,6 +128,17 @@ class FastTextDataLoader:
         Returns:
             tuple: A tuple containing two NumPy arrays: X (preprocessed text data) and y (encoded genre labels).
         """
-        pass
+        # df = self.read_data_to_df()
+        # read from file
+        df = pd.read_csv('./training_data.csv')
+        texts = df['synopsis'] + ' ' + df['summary'] + ' ' + df['reviews']  # Concatenate text data
+        labels = df['genre']
+        return texts, labels
 
 
+if __name__ == "__main__":
+    path = path_access.path_to_logic() + "core/indexer/saved_indexes/"
+    print(f"path is {path}")
+    ftdl = FastTextDataLoader(path)
+    df = ftdl.read_data_to_df()
+    # df.to_csv("training_data.csv")
--- Logic/core/word_embedding/fasttext_model.py
+++ Logic/core/word_embedding/fasttext_model.py
@@ -6,32 +6,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from scipy.spatial import distance
 
-from .fasttext_data_loader import FastTextDataLoader
+from Logic.core import path_access
+from Logic.core.word_embedding.fasttext_data_loader import FastTextDataLoader
+from Logic.core.word_embedding.fasttext_data_loader import preprocess_text
 
-
-def preprocess_text(text, minimum_length=1, stopword_removal=True, stopwords_domain=[], lower_case=True,
-                       punctuation_removal=True):
-    """
-    preprocess text by removing stopwords, punctuations, and converting to lowercase, and also filter based on a min length
-    for stopwords use nltk.corpus.stopwords.words('english')
-    for punctuations use string.punctuation
-
-    Parameters
-    ----------
-    text: str
-        text to be preprocessed
-    minimum_length: int
-        minimum length of the token
-    stopword_removal: bool
-        whether to remove stopwords
-    stopwords_domain: list
-        list of stopwords to be removed base on domain
-    lower_case: bool
-        whether to convert to lowercase
-    punctuation_removal: bool
-        whether to remove punctuations
-    """
-    pass
 
 class FastText:
     """
@@ -57,17 +35,19 @@ class FastText:
         self.method = method
         self.model = None
 
-
-    def train(self, texts):
+    def train(self, texts_path, epochs=5):
         """
         Trains the FastText model with the given texts.
 
         Parameters
         ----------
-        texts : list of str
-            The texts to train the FastText model.
+        epochs : int
+            How many epoches to train the fasttext unsupervised training
+        texts_path : str
+            Address to the training file of the FastText model.
         """
-        pass
+        self.model = fasttext.train_unsupervised(input=texts_path, model=self.method, epoch=epochs)
+        # pass
 
     def get_query_embedding(self, query):
         """
@@ -87,7 +67,8 @@ class FastText:
         np.ndarray
             The embedding for the query.
         """
-        pass
+        return self.model.get_sentence_vector(query)
+        # pass
 
     def analogy(self, word1, word2, word3):
         """
@@ -101,6 +82,9 @@ class FastText:
         Returns:
             str: The word that completes the analogy.
         """
+        analogies = self.model.get_analogies(word1, word2, word3)
+        print(analogies)
+        return analogies[0][1]
         # Obtain word embeddings for the words in the analogy
         # TODO
 
@@ -115,7 +99,39 @@ class FastText:
 
         # Find the word whose vector is closest to the result vector
         # TODO
-        pass
+        passI
+
+    def detailed_analogy(self, word1, word2, word3):
+        """
+        Perform an analogy task: word1 is to word2 as word3 is to __.
+
+        Args:
+            word1 (str): The first word in the analogy.
+            word2 (str): The second word in the analogy.
+            word3 (str): The third word in the analogy.
+
+        Returns:
+            str: The word that completes the analogy.
+        """
+        # Obtain word embeddings for the words in the analogy
+        embeddings_word1 = self.model[word1]
+        embeddings_word2 = self.model[word2]
+        embeddings_word3 = self.model[word3]
+
+        analogy_vector = embeddings_word2 - embeddings_word1 + embeddings_word3
+        word_embeddings = {word: self.model[word] for word in self.model.words}
+
+        candidate_words = set(self.model.words) - {word1, word2, word3}  # don't use input words for candidate words !
+
+        closest_word = None
+        min_distance = float('inf')
+        for word in candidate_words:
+            distance_to_analogy_vector = distance.cosine(word_embeddings[word], analogy_vector)
+            if distance_to_analogy_vector < min_distance:
+                min_distance = distance_to_analogy_vector
+                closest_word = word
+
+        return closest_word
 
     def save_model(self, path='FastText_model.bin'):
         """
@@ -126,9 +142,10 @@ class FastText:
         path : str, optional
             The path to save the FastText model.
         """
-        pass
+        self.model.save_model(path)
+        # pass
 
-    def load_model(self, path="FastText_model.bin"):
+    def load_model(self, path="./FastText_model.bin"):
         """
         Loads the FastText model from a file.
 
@@ -137,36 +154,61 @@ class FastText:
         path : str, optional
             The path to load the FastText model.
         """
-        pass
+        self.model = fasttext.load_model(path)
+        # pass
 
-    def prepare(self, dataset, mode, save=False, path='FastText_model.bin'):
+    def prepare(self, dataset_path, mode, epochs=5, save=False, path='./FastText_model.bin'):
         """
         Prepares the FastText model.
 
         Parameters
         ----------
-        dataset : list of str
-            The dataset to train the FastText model.
+        epochs : int
+            How many epoches to train
+        dataset_path : str
+            Address to the training data of the FastText model.
         mode : str
             The mode to prepare the FastText model.
         """
         if mode == 'train':
-            self.train(dataset)
+            self.train(dataset_path, epochs=epochs)
         if mode == 'load':
             self.load_model(path)
         if save:
             self.save_model(path)
 
+
 if __name__ == "__main__":
-    ft_model = FastText(preprocessor=preprocess_text, method='skipgram')
 
-    path = './Phase_1/index/'
-    ft_data_loader = FastTextDataLoader()
+    # importing non-preprocessed training data
 
-    X = ft_data_loader.create_train_data(path)
+    path = path_access.path_to_logic() + "core/indexer/saved_indexes/"
+    ft_data_loader = FastTextDataLoader(path)
+    texts, _ = ft_data_loader.create_train_data()
+    print(f"training dataset loaded successfully")
 
-    ft_model.train(X)
-    ft_model.prepare(None, mode = "save")
+    # preprocessing training data and saving it
+
+    # preprocessed_texts = [preprocess_text(text) for text in texts]
+    # with open('preprocessed_training_data.txt', 'w', encoding="utf-8") as f:
+    #     for line in preprocessed_texts:
+    #         f.write("%s\n" % line)
+    # print(f"training dataset preprocessed and saved successfully")
+
+    ft_model = FastText(method='skipgram')
+
+    # loading model if it has already been trained (bin file)
+    # ft_model.load_model()
+    # ft_model.prepare(None, mode="load")
+
+    # training model
+    # ft_model.train("./preprocessed_training_data.txt")
+    ft_model.prepare("./preprocessed_training_data.txt", mode="train", epochs=30)
+    print(f"model trained successfully")
+
+    # saving model
+    ft_model.prepare(None, mode="save", save=True)
+    print(f"model saved successfully")
 
     print(10 * "*" + "Similarity" + 10 * "*")
     word = 'queen'
@@ -179,4 +221,7 @@ if __name__ == "__main__":
     word1 = "man"
     word2 = "king"
     word3 = "queen"
-    print(f"Similarity between {word1} and {word2} is like similarity between {word3} and {ft_model.analogy(word1, word2, word3)}")
+    print(
+        f"Similarity between {word1} and {word2} is like similarity between {word3} and {ft_model.analogy(word1, word2, word3)}")
+    print(
+        f"Similarity between {word1} and {word2} is like similarity between {word3} and {ft_model.detailed_analogy(word1, word2, word3)}")
